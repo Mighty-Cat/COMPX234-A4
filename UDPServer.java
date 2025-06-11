58bfe59766ec14826a8f6728b646a2dee7dd2f51
MightyCat <3300190576@qq.com>
2025-06-11 09:49:49 +0800

    Add DOWNLOAD request handling in server with OK/ERR responses
    - Parsed DOWNLOAD <filename> messages
    - Checked file existence and sent OK with size or ERR NOT_FOUND
    - Responded to client using the same socket

--- UDPServer.java
+++ UDPServer.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 
 public class UDPServer {
@@ -25,7 +25,25 @@ public class UDPServer {
                 socket.receive(packet);
                 String received = new String(packet.getData(), 0, packet.getLength()).trim();
                 System.out.println("Received: " + received);
-                // TODO: Process request
+
+                String[] parts = received.split(" ");
+                if (parts.length >= 2 && parts[0].equals("DOWNLOAD")) {
+                    String filename = parts[1];
+                    File file = new File(filename);
+                    String response;
+                    if (file.exists()) {
+                        // Temporary OK response, will add port later
+                        response = "OK " + filename + " SIZE " + file.length();
+                    } else {
+                        response = "ERR " + filename + " NOT_FOUND";
+                    }
+                    byte[] sendData = response.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(
+                        sendData, sendData.length, packet.getAddress(), packet.getPort()
+                    );
+                    socket.send(sendPacket);
+                    System.out.println("Sent: " + response);
+                }
             }
         } catch (IOException e) {
             System.out.println("Server error: " + e.getMessage());
